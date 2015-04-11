3396f289bca210ded6c9925f483d0ce4dd5e78ee
Britney Wright <blw06g@gmail.com>
2015-04-11 12:27:29 -0400

    move contributor logic from view to helper

--- app/helpers/github_helper.rb
+++ app/helpers/github_helper.rb
@@ -16,13 +16,20 @@ module GithubHelper
     end
   end
   
-  def individual_contributor(hmm)
-   contributors = exercism_contributors
-   contributors.flatten.select{|x| x["contributor"].login == hmm}
+  def individual_contributor(name)
+   exercism_contributors.flatten.select{|x| x["contributor"].login == name}
+  end
+
+  def unique_contributor(name)
+   individual_contributor(name).first["contributor"] 
+  end
+
+  def unique_contributors
+    exercism_contributors.flatten.map{|contributor| contributor["contributor"]}.uniq{|x| x[:login]}
   end
 
   def render_contributors
-    ec = exercism_contributors.flatten.map{|contributor| contributor["contributor"]}.uniq{|x| x[:login]}
+    ec = unique_contributors 
     render :partial => '/layouts/github_contributors', :locals => {:contributors_list => ec}
   end 
 
